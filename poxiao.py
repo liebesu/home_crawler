518ddfe6c7206a90a81e607cc250b9c66f19ca71
liebesu <lichunjiang42@gmail.com>
2016-08-24 10:02:25 +0800

    update

--- poxiao.py
+++ poxiao.py
@@ -1,8 +1,27 @@
+import os
+import re
 import requests
 from bs4 import BeautifulSoup
 import ConfigParser
+from lib.common.constants import HOME_CRAWLER_ROOT
 def db_insert():
     pass
-def __name__=="__main__":
+def readconf():
+    file_conf_path=os.path.join(HOME_CRAWLER_ROOT,"conf","movie.conf")
+    rf=ConfigParser.ConfigParser()
+    rf.read(file_conf_path)
+    url=rf.get("movie","url")
+    serch_word=rf.get("movie","serch_word")
+    return url,serch_word
+def get_info():
+    url,serch_word=readconf()
+    r=requests.get(url)
+    soup=BeautifulSoup(r.content,"html.parser")
+    movie_lists=soup.find_all(href=re.compile(serch_word))
+    for movie_info in movie_lists:
+        print movie_info.text
+if __name__=="__main__":
+    get_info()
+    
     
     
